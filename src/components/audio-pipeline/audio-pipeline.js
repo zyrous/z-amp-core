c5c65d03c028db58876fc07a601781517cfd3fad
Mason Yarrick <masonyarrick@gmail.com>
2021-03-08 14:17:56 +0800

    chore: fixing parseInt usage

--- src/components/audio-pipeline/audio-pipeline.js
+++ src/components/audio-pipeline/audio-pipeline.js
@@ -117,14 +117,14 @@ class AudioPipeline extends AudioComponent {
         for(var i=0; i<this.pipelineSegments.length; i++){
 
             // Find the segments on either side of this one.
-            previousSegment = this.pipelineSegments[parseInt(i)];
-            nextSegment = this.pipelineSegments[parseInt(i)+1];
+            previousSegment = this.pipelineSegments[parseInt(i, 10)];
+            nextSegment = this.pipelineSegments[parseInt(i, 10)+1];
             
             if(previousSegment && previousSegment.weighting <= weighting
                 && nextSegment && nextSegment.weighting >= weighting) {
 
                 // We're inbetween two different weightings, so we've found the right place.
-                segmentIndex = parseInt(i)+1;
+                segmentIndex = parseInt(i, 10)+1;
                 break;
             }
         }
