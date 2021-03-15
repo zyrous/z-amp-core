b8348f92451dec775d06c551ebbd2a75d7bbce9a
Mason Yarrick <mason.yarrick@zyrous.com>
2021-03-15 11:40:09 +0800

    fix: changing transpilation options

--- src/exports.js
+++ src/exports.js
@@ -5,6 +5,9 @@
  * @author Mason Yarrick <mason.yarrick@zyrous.com>
  */
 
+const corejs = require("core-js");
+const regenerator = require("regenerator-runtime/runtime");
+
 // Get the existing WebAmp object, or create one if it doesn't exist.
 var ZAmp = window.ZAmp || {};
 const { Amp } = require("./components/amp/amp"); 
